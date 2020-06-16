9b73fd5baca2b69a23cf5448be9b7b422cb6acd0
WeissWolf <40091601+ashkanmy@users.noreply.github.com>
2020-06-16 16:00:50 +0200

    Update What_is_rValue.cpp

--- What_is_rValue.cpp
+++ What_is_rValue.cpp
@@ -7,6 +7,11 @@
      cout << "Here is my integer-> "<< My_rVal << endl;
  };
  
+ // Here, 
+ //   (i) I do not need to define many variable of type int and assign them all with 12! (This way is more efficient)
+ //   (ii) The argument of UseMe is a r-value. Dissimilar to l-value referencing, any change done on My_rVal does not 
+ //        impact its value out of the scope of the UseMe function. This could have happened if My_rVal was passed by 
+ //        l-value reference and not with r-value reference.
  int main(){
      UseMe(12+12+12+12+12+12+12+12+12+12+12+1+12+12+12+12+12+12+12+12);
      return 0;
