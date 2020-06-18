1671dbbdf32e07c78aeb4f9c124bc76592379e04
WeissWolf <40091601+ashkanmy@users.noreply.github.com>
2020-06-18 08:07:53 +0200

    Update PassUniquePtrToFunction.cpp

--- PassUniquePtrToFunction.cpp
+++ PassUniquePtrToFunction.cpp
@@ -10,9 +10,9 @@ using std::move;
 
 class Human{
     public:
-    Human(string name):name_(name){cout<<name_;}
+    Human(string name):name_(name){}
     void PrintAddresses(){
-        cout << " --> Objekt--> " << this << " --> member--> " << &name_ << endl;  
+        cout << " --> Objekt (on heap) --> " << this << " --> member  (on heap) --> " << name_ << endl;  
     }
     private:
        string name_;
@@ -27,6 +27,7 @@ void PassOwnerShip(unique_ptr<Human> up_human)
 int main()
 { 
     //unique_ptr<Human> ash(new Human("Ash"));
-    unique_ptr<Human> ash = make_unique<Human>("Ash");
+    unique_ptr<Human> ash = make_unique<Human>("Ash"); 
+    cout << "unique_ptr address (on stack) ---> " << &ash;
     PassOwnerShip(move(ash));
 }
