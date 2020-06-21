6232c207e7dff3aa818d51e0de79a5c09df4cb8b
WeissWolf <40091601+ashkanmy@users.noreply.github.com>
2020-06-21 07:27:53 +0200

    Create Thread_vs_Async.cpp

---
+++ Thread_vs_Async.cpp
@@ -0,0 +1,21 @@
+#include <future>
+#include <iostream>
+
+int show(int mn){
+//void show(std::promise<int>&& p,int mn){
+    mn++;
+    return mn; //p.set_value(mn);
+}
+
+int main(){    
+    int myNumber=10;
+    int myNumberUpdated;
+    //std::promise<int> p;
+    std::future<int> f=std::async(show,myNumber);   //std::future<int> f=p.get_future();
+    //std::thread t(show,std::move(p),myNumber);
+    myNumberUpdated=f.get();
+    std::cout<<"update->"<<myNumberUpdated<<std::endl;
+    //t.join();
+    return 0;
+}
+ 
