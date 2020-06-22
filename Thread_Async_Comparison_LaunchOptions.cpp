7e1f399fe930ad1925bee7272f581fe279e27624
WeissWolf <40091601+ashkanmy@users.noreply.github.com>
2020-06-22 09:06:01 +0200

    Update Thread_Async_Comparison_LaunchOptions.cpp

--- Thread_Async_Comparison_LaunchOptions.cpp
+++ Thread_Async_Comparison_LaunchOptions.cpp
@@ -3,9 +3,11 @@
 #include <thread>
 #include <cmath>
 #include <vector>
-void DoIt(int i){
-    std::cout<<std::this_thread::get_id()<<std::endl;
+void DoIt(int i,int thr_no){
+    std::cout<<"Thread id-> "<<std::this_thread::get_id()<<std::endl;
     for(int c=1;c<i;c++){sqrt(81);}
+    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+    std::cout<<"Thread no-> "<<thr_no<<std::endl;
 };
 int main(){
    int loop=10;
@@ -16,8 +18,11 @@ int main(){
    std::vector<std::future<void>> futures;
    for(int nThread=0;nThread<Threads;nThread++)
    {
-       //futures.emplace_back(std::async(std::launch::async,DoIt,loop));
-       futures.emplace_back(std::async(std::launch::deferred,DoIt,loop));
+       //---->  std::launch::async	     ---->    a new thread is launched to execute the task asynchronously
+       //---->  std::launch::deferred	 ---->    the task is executed on the already called (main) thread (lazy evaluation)
+       
+       futures.emplace_back(std::async(std::launch::async,DoIt,loop,nThread)); 
+       //futures.emplace_back(std::async(std::launch::deferred,DoIt,loop,nThread));
        //futures.emplace_back(std::async(std::launch::any,DoIt,loop));
    }
    for(std::future<void> &f:futures)
@@ -26,4 +31,3 @@ int main(){
    std::cout << "nLoops-> " <<loop <<" nThreads->" << Threads << " microseconds-> " << std::chrono::duration_cast<std::chrono::microseconds>( Ty - Tx ).count()<<std::endl;
    return 0;
 }
-
