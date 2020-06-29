8128af147575249a0fa24e49b83dadc73843fd76
AshMansouri <40091601+ashkanmy@users.noreply.github.com>
2020-06-29 08:10:21 +0200

    Create Thread_Packaged_Task.cpp

---
+++ Thread_Packaged_Task.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <functional>
+#include <future>
+
+int main(){
+    
+    std::packaged_task<int(int,int)> task([](int alpha,int beta){
+                                                                    return (alpha+beta); 
+                                                                }
+                                         );
+    std::future<int> f=task.get_future();
+    task(3,4);
+    std::cout<<"S->"<<f.get()<<std::endl;
+    
+    return 0;
+}
