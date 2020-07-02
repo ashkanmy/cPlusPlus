732cc4dd499ee009f1a80c0ea5a4077c652b5904
AshMansouri <40091601+ashkanmy@users.noreply.github.com>
2020-07-02 09:06:44 +0200

    Update Lambda_CaptureByReference_Mutable.cpp

--- Lambda_CaptureByReference_Mutable.cpp
+++ Lambda_CaptureByReference_Mutable.cpp
@@ -4,7 +4,7 @@ int main(){
     
     /* define the lambda*/
     int value = 0;
-    auto temp = [&value]()mutable{value++;};
+    auto temp = [&value](){value++;};
     temp();
     std::cout<<value<<std::endl; 
     return 0;
